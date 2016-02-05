9ed57da03676e7ee849eeb2d83433f846b6965db
Matt Basta <basta@uber.com>
2016-02-05 10:47:36 -0800

    Turn on uglify

--- webpack.config.js
+++ webpack.config.js
@@ -20,11 +20,11 @@ module.exports = {
     },
     plugins: [
         new ExtractTextPlugin('style.css'),
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {warnings: false},
-        //     mangle: {},
-        //     sourceMap: false,
-        // }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {warnings: false},
+            mangle: {},
+            sourceMap: false,
+        }),
         new webpack.optimize.DedupePlugin(),
     ],
     module: {
