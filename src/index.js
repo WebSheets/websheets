b0df713f5355df571311b104d0eb3c2b024738a1
Matt Basta <mattbasta@gmail.com>
2016-03-04 10:09:58 -0800

    Export more utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+import Emitter from './Emitter';
+import {getCellID, getCellPos} from './utils/cellID';
 import parseExpression from './exprCompiler';
 import WebSheet from './WebSheet';
 import WebSheetContext from './WebSheetContext';
 
-WebSheet.parseExpression = parseExpression;
-WebSheet.WebSheetContext = WebSheetContext;
 
 // This is because `export default` exports {default: WebSheet}
 module.exports = WebSheet;
+
+export {
+    Emitter,
+    getCellID,
+    getCellPos,
+    parseExpression,
+    WebSheetContext,
+};
